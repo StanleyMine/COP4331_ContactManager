54171b99551d6aba5a56dcac923ec9a77fa756c3
Stanley <staanleyminervini@gmail.com>
2020-09-08 03:53:45 -0400

    Implement Addrow

--- contactManager.js
+++ contactManager.js
@@ -13,6 +13,35 @@ async function searchContacts() {
   }, 5000);
 }
 
+function addRow(dataRow) {
+  const mytable = document.getElementById("contact-table");
+  const row = document.createElement("tr");
+  const columnKeys = [
+    "firstName",
+    "lastName",
+    "email",
+    "phoneNumber",
+    "skill",
+    "dateAdded",
+  ];
+  // loop through dataRow and add to row
+  for (const columnKey of columnKeys) {
+    const rowCell = document.createElement("td");
+    rowCell.innerText = dataRow[columnKey];
+    row.appendChild(rowCell);
+  }
+
+  const deleteCell = document.createElement("td");
+  deleteCell.innerText = "🗑️";
+  deleteCell.addEventListener("click", async () => {
+    await deleteRowRequest(dataRow.id); // you need to make this
+    row.remove(); // detach the row if and when the request gets a success response
+  });
+  row.appendChild(deleteCell);
+
+  table.appendChild(row);
+}
+
 async function deleteContact() {}
 
 async function createContact() {}
