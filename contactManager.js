8f27b973c36354f9015756516552483d43cb1e0d
Stanley <staanleyminervini@gmail.com>
2020-09-12 06:36:57 -0400

    Update Contact Manager

--- contactManager.js
+++ contactManager.js
@@ -3,33 +3,57 @@ document
   .addEventListener("submit", searchContacts);
 
 async function searchContacts() {
-  const name = document.getElementById("name-input").value;
-  const skill = document.getElementById("skill-input").value;
-  // need to check values for separate end points
-  const response = await apiRequest(
-    "/LAMPAPI/searchContact.php",
-    { name, skill },
-    "POST"
-  );
+  const searchType = document.getElementById("select-search").value;
+  const searchCriteria = document.getElementById("search-input").value;
+  const id = document.cookie;
+  const myTbody = document.getElementById("tableTbody");
+  let path;
+  let data = { id };
+  if (!searchCriteria) {
+    // checking search input
+    errorMessage("Please provide search criteria.");
+    return;
+  }
 
-  for (const contact of response.data) {
-    addRow(contact);
+  if (searchType == "Name") {
+    path = "search_by_name";
+    data = { id, name: searchCriteria };
+  } else if (searchType == "Skill") {
+    path = "search_skills";
+    data = { id, skill: searchCriteria };
+  }
+
+  const response = await apiRequest("/LAMPAPI/" + path + ".php", data, "POST");
+  if (response == 200) {
+    if (response.data.error) {
+      errorMessage(response.data.error);
+      return;
+    }
+    myTbody.innerHTML = "";
+    for (const contact of response.data) {
+      addRow(contact);
+    }
+  } else {
+    errorMessage("Something went wrong, try again later.");
   }
 }
 
 function addRow(dataRow) {
-  const myTable = document.getElementById("contact-table");
+  const myTable = document
+    .getElementById("contact-table")
+    .getElementsByTagName("tbody")[0];
   const row = document.createElement("tr");
   const columnKeys = [
-    "firstName",
-    "lastName",
+    "fullName",
     "email",
     "phoneNumber",
-    "skill",
-    "dateAdded",
+    "skills",
+    "projectLink",
+    "dateAdded", // TODO : need to check this, no api endpoint seems to corroborate this
   ];
   // loop through dataRow and add to row
   for (const columnKey of columnKeys) {
+    // not convinced this works
     const rowCell = document.createElement("td");
     rowCell.innerText = dataRow[columnKey];
     row.appendChild(rowCell);
@@ -38,7 +62,7 @@ function addRow(dataRow) {
   const deleteCell = document.createElement("td");
   deleteCell.innerText = "🗑️";
   deleteCell.addEventListener("click", async () => {
-    const succeeded = await deleteRowRequest(dataRow.id);
+    const succeeded = await deleteRowRequest(dataRow.phoneNumber);
     if (succeeded) {
       row.remove();
     }
@@ -48,11 +72,13 @@ function addRow(dataRow) {
   myTable.appendChild(row);
 }
 
-async function deleteRowRequest(dataRow) {
+async function deleteRowRequest(phoneNumber) {
+  const id = document.cookie;
+
   const response = await apiRequest(
     "/LAMPAPI/deleteContact.php",
-    { id },
-    "DELETE"
+    { id, phoneNumber },
+    "POST"
   );
 
   if (response.status != 200) {
@@ -64,5 +90,3 @@ async function deleteRowRequest(dataRow) {
   }
   return true;
 }
-
-async function createContact() {}
